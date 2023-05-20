e123ea2fb946063b04392cb037d1d0ad1a7e5b75
Sarvesh <69846655+sarvesh2001@users.noreply.github.com>
2023-05-20 18:20:03 +0530

    Update Student.java
    adding Student value in variable to print that

--- src/oop/object_class/Student.java
+++ src/oop/object_class/Student.java
@@ -10,6 +10,9 @@ public class Student {
 	public static void main(String args[]) {
 		//creating an object or instance
 		Student s1 = new Student();//creating an object of student
+		//seting
+		s1.id = 1;
+		s1.name = "hello-world";
 		//Printing values of the object
 		System.out.println(s1.id);//accessing member through reference variable
 		System.out.println(s1.name);
